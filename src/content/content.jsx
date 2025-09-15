cc811f0c51d956e5920748ca20728cbf5e721676
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-09-15 14:54:44 -0300

    fix: Icons

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -211,11 +211,10 @@ const content = {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-users-icon lucide-users"
+            class="lucide lucide-user-check-icon lucide-user-check"
           >
+            <path d="m16 11 2 2 4-4" />
             <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-            <path d="M16 3.128a4 4 0 0 1 0 7.744" />
-            <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
             <circle cx="9" cy="7" r="4" />
           </svg>
         ),
@@ -246,10 +245,11 @@ const content = {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-user-check-icon lucide-user-check"
+            class="lucide lucide-users-icon lucide-users"
           >
-            <path d="m16 11 2 2 4-4" />
             <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+            <path d="M16 3.128a4 4 0 0 1 0 7.744" />
+            <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
             <circle cx="9" cy="7" r="4" />
           </svg>
         ),
